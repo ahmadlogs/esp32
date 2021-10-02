f7b02736b4afd915b2ac9524625ecc3d4a5ab66b
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2021-10-02 15:27:27 +0500

    Add files via upload

---
+++ blynk2-relay/blynk2-relay.ino
@@ -0,0 +1,207 @@
+/************************************************************************************
+ *  Created By: Tauseef Ahmad
+ *  Created On: 30 September, 2021
+ *  
+ *  YouTube Video: https://youtu.be/DshR6Y9aTSs
+ *  My Channel: https://www.youtube.com/channel/UCOXYfOHgu-C-UfGyDcu5sYw/
+ *  
+ *  *********************************************************************************
+ *  Preferences--> Aditional boards Manager URLs : 
+ *  For ESP32:
+ *  https://dl.espressif.com/dl/package_esp32_index.json
+ *  
+ *  *********************************************************************************
+ *  Download Blynk Library (Version 1.0.1):
+ *  https://github.com/blynkkk/blynk-library
+ **********************************************************************************/
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "ENTER_TEMPLATE_ID"
+#define BLYNK_DEVICE_NAME "ENTER_DEVICE_NAME"
+#define BLYNK_AUTH_TOKEN "ENTER_AUTH_TOKEN"
+
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ENTER_WIFI_SSID";
+char pass[] = "ENTER_WIFI_PASSWORD";
+
+BlynkTimer timer;
+
+
+#define button1_pin 26
+#define button2_pin 25
+#define button3_pin 33
+#define button4_pin 32
+
+#define relay1_pin 13
+#define relay2_pin 12
+#define relay3_pin 14
+#define relay4_pin 27
+
+int relay1_state = 0;
+int relay2_state = 0;
+int relay3_state = 0;
+int relay4_state = 0;
+
+
+//Change the virtual pins according the rooms
+#define button1_vpin    V1
+#define button2_vpin    V2
+#define button3_vpin    V3 
+#define button4_vpin    V4
+
+//------------------------------------------------------------------------------
+// This function is called every time the device is connected to the Blynk.Cloud
+// Request the latest state from the server
+BLYNK_CONNECTED() {
+  Blynk.syncVirtual(button1_vpin);
+  Blynk.syncVirtual(button2_vpin);
+  Blynk.syncVirtual(button3_vpin);
+  Blynk.syncVirtual(button4_vpin);
+}
+
+//--------------------------------------------------------------------------
+// This function is called every time the Virtual Pin state change
+//i.e when web push switch from Blynk App or Web Dashboard
+BLYNK_WRITE(button1_vpin) {
+  relay1_state = param.asInt();
+  digitalWrite(relay1_pin, relay1_state);
+}
+//--------------------------------------------------------------------------
+BLYNK_WRITE(button2_vpin) {
+  relay2_state = param.asInt();
+  digitalWrite(relay2_pin, relay2_state);
+}
+//--------------------------------------------------------------------------
+BLYNK_WRITE(button3_vpin) {
+  relay3_state = param.asInt();
+  digitalWrite(relay3_pin, relay3_state);
+}
+//--------------------------------------------------------------------------
+BLYNK_WRITE(button4_vpin) {
+  relay4_state = param.asInt();
+  digitalWrite(relay4_pin, relay4_state);
+}
+//--------------------------------------------------------------------------
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  //--------------------------------------------------------------------
+  pinMode(button1_pin, INPUT_PULLUP);
+  pinMode(button2_pin, INPUT_PULLUP);
+  pinMode(button3_pin, INPUT_PULLUP);
+  pinMode(button4_pin, INPUT_PULLUP);
+  //--------------------------------------------------------------------
+  pinMode(relay1_pin, OUTPUT);
+  pinMode(relay2_pin, OUTPUT);
+  pinMode(relay3_pin, OUTPUT);
+  pinMode(relay4_pin, OUTPUT);
+  //--------------------------------------------------------------------
+  //During Starting all Relays should TURN OFF
+  digitalWrite(relay1_pin, HIGH);
+  digitalWrite(relay2_pin, HIGH);
+  digitalWrite(relay3_pin, HIGH);
+  digitalWrite(relay4_pin, HIGH);
+  //--------------------------------------------------------------------
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+  //--------------------------------------------------------------------
+  Blynk.virtualWrite(button1_vpin, relay1_state);
+  Blynk.virtualWrite(button2_vpin, relay2_state);
+  Blynk.virtualWrite(button3_vpin, relay3_state);
+  Blynk.virtualWrite(button4_vpin, relay4_state);
+  //--------------------------------------------------------------------
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+  
+  listen_push_buttons();
+}
+
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+void listen_push_buttons(){
+    //--------------------------------------------------------------------------
+    if(digitalRead(button1_pin) == LOW){
+      delay(200);
+      control_relay(1);
+      Blynk.virtualWrite(button1_vpin, relay1_state); //update button state
+    }
+    //--------------------------------------------------------------------------
+    else if (digitalRead(button2_pin) == LOW){
+      delay(200);
+      control_relay(2);
+      Blynk.virtualWrite(button2_vpin, relay2_state); //update button state
+    }
+    //--------------------------------------------------------------------------
+    else if (digitalRead(button3_pin) == LOW){
+      delay(200);
+      control_relay(3);
+      Blynk.virtualWrite(button3_vpin, relay3_state); //update button state
+    }
+    //--------------------------------------------------------------------------
+    else if (digitalRead(button4_pin) == LOW){
+      delay(200);
+      control_relay(4);
+      Blynk.virtualWrite(button4_vpin, relay4_state); //update button state
+    }
+    //--------------------------------------------------------------------------
+}
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+
+
+
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+void control_relay(int relay){
+  //------------------------------------------------
+  if(relay == 1){
+    relay1_state = !relay1_state;
+    digitalWrite(relay1_pin, relay1_state);
+    Serial.print("Relay1 State = ");
+    Serial.println(relay1_state);
+    delay(50);
+  }
+  //------------------------------------------------
+  else if(relay == 2){
+    relay2_state = !relay2_state;
+    digitalWrite(relay2_pin, relay2_state);
+    delay(50);
+  }
+  //------------------------------------------------
+  else if(relay == 3){
+    relay3_state = !relay3_state;
+    digitalWrite(relay3_pin, relay3_state);
+    delay(50);
+  }
+  //------------------------------------------------
+  else if(relay == 4){
+    relay4_state = !relay4_state;
+    digitalWrite(relay4_pin, relay4_state);
+    delay(50);
+  }
+  //------------------------------------------------
+}
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
